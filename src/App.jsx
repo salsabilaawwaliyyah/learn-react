974936b9a1bd0e9dc07313e16afecb1d9ba67651
Cacaa <salsabilaawwaliyyahl@gmail.com>
2024-08-28 14:08:16 +0700

    Baru

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import Navbar from "./components/navbar"
+
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hai Love!!
-    </h1>
+    // <h1 className="text-3xl font-bold underline">
+    //   Hai Love!!
+    // </h1>
+    <div className= 'bg-gray-100 min-j-scream'>
+          <Navbar />
+    </div>
   )
 }
 
